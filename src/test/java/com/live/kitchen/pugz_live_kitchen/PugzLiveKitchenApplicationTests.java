b7d942f4210e775efe243bd51d25c920892dfabd
Pughazh B <bpughazh@gmil.com>
2022-12-27 19:44:04 +0530

    commit test

---
+++ src/test/java/com/live/kitchen/pugz_live_kitchen/PugzLiveKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.live.kitchen.pugz_live_kitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PugzLiveKitchenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
